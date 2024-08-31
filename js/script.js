542d5946f50f29d6a7bd7efbefc6b0166e1f7a18
jochefini <mjgcoppola@gmail.com>
2024-08-31 15:39:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function showData(dataArray) {
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
 fetch(DATA_URL)
-  .then(response => {
+  .then((response) => {
     
     if (!response.ok) {
       throw new Error('Network response was not ok');
